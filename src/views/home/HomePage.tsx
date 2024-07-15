53ac6ced304e42462d3cdedaa0cdb510120cb199
Kovaleva Marina <kirienko1995@mail.ru>
2024-07-15 15:09:40 +0300

    feat: Add section Recipe Dishes

--- src/views/home/HomePage.tsx
+++ src/views/home/HomePage.tsx
@@ -1,5 +1,7 @@
 import {BannerSlider, CategorySlider, Title} from "../../components/common";
 import {pattern_one} from "../../utils/images.ts";
+import {dishTypeData} from "../../data.ts";
+import {Link} from "react-router-dom";
 
 const HomePage = () => {
     return (
@@ -9,9 +11,34 @@ const HomePage = () => {
                 background: `url:('${pattern_one}') center-cover no-repeat`
             }}>
                 <div className="container">
-                    <Title subTitle='Choose a Category' mainTitle='Recipe Categories' />
+                    <Title subTitle='Choose a Category' mainTitle='Recipe Categories'/>
                 </div>
                 <CategorySlider/>
+            </section>
+            <section className='showcase-recipes'>
+                <div className="container">
+                    <Title subTitle='Some Recipies' mainTitle='Chicken Recipies'/>
+                </div>
+            </section>
+            <section className='dishes'>
+                <div className="container">
+                    <Title subTitle='Find Dishes you love' mainTitle='Recipe Dishes'/>
+                    <div className="dishes-list">
+                        {
+                            dishTypeData?.map((dish, index) => (
+                                <Link to={`recipies/dish/${dish?.type}`}
+                                      key={index}
+                                      className='dishes-item'
+                                >
+                                    <img src={dish.image} alt={dish?.type}/>
+                                    <p className='dishes-item-name'>
+                                        {dish?.type}    
+                                    </p>
+                                </Link>
+                            ))
+                        }
+                    </div>
+                </div>
             </section>
         </main>
     );
